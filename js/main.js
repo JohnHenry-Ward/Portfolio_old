6df55508f1dcc8424f335eab9c5775997b65c149
JohnHenry <johnhenry514@gmail.com>
2019-02-26 17:15:19 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+
+    //Menu animation open
+    $('#open-menu').click(function(){
+        $('#open-menu').animate({
+            right: '-30px'
+        });
+
+        $('.menuBackground').animate({
+            width: 'hide'
+        });
+        
+        $('.menu').delay('500').animate({
+            width: 'show'
+        });
+
+        $('li').hide().delay('600').fadeIn('slow');
+
+    });
+
+    //Menu animation close
+    $('#closeMenu').click(function(){
+        $('.menu').animate({
+            width: 'hide'
+        });
+
+        $('#open-menu').delay('500').animate({
+            right: '10px'
+        });
+
+        $('.menuBackground').delay('500').animate({
+            width: 'show'
+        });
+
+    })
+
+    //Smooth Scroll
+    $('.scroll').click(function (e) {
+        e.preventDefault();
+
+        $('html,body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    });
+
+
+});
