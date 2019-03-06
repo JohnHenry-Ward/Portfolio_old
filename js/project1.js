d7c97a3856cefa38128aa2cd3dc39544611df06c
JohnHenry <johnhenry514@gmail.com>
2019-03-05 21:46:31 -0800

    added lightbox to project1.html

---
+++ js/project1.js
@@ -0,0 +1,7 @@
+$(document).ready(function (){
+    baguetteBox.run('.images', {
+        animation: 'fadeIn',
+        noScrollbars: true,
+        overlayBackgroundColor: 'rgba(47, 50, 52, 0.5)'
+    });
+});
