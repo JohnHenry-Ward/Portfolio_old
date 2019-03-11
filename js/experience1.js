f1ba205748c6890a673600162263f8eaaacb1b8a
JohnHenry <johnhenry514@gmail.com>
2019-03-10 18:56:35 -0700

    updated layout and media queires

---
+++ js/experience1.js
@@ -0,0 +1,7 @@
+$(document).ready(function (){
+    baguetteBox.run('.imageGrid', {
+        animation: 'fadeIn',
+        noScrollbars: true,
+        overlayBackgroundColor: 'rgba(47, 50, 52, 0.5)'
+    });
+});
