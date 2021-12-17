59ac348735b53f8946a0da7e756c20984133e181
JohnHenry <johnhenry514@gmail.com>
2021-12-16 19:14:26 -0800

    remove unused JS

--- js/experience1.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function (){
-    baguetteBox.run('.imageGrid', {
-        animation: 'fadeIn',
-        noScrollbars: true,
-        overlayBackgroundColor: 'rgba(47, 50, 52, 0.5)'
-    });
-});
